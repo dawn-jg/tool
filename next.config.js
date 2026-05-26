24d98ecbc452bd90a82ef78fa1b54b2314783250
dawn-jg <dawn-jg@github.com>
2026-05-26 11:37:36 +0800

    cleanup: remove dangling webpack alias for async_hooks

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.resolve.alias = config.resolve.alias || {};
-      config.resolve.alias['async_hooks'] = path.resolve(__dirname, 'scripts/async-hooks-polyfill.js');
-    }
-    return config;
   },
 };
 
